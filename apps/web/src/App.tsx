02d80dc5097b51ae2edad2ddb0c0b3206b589c6f
MouadBNL <mouadbnl.2k01@gmail.com>
2025-06-27 13:37:14 +0100

    feat(web): setup tailwind and shadcn

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { HealthRepository } from './repositories'
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,8 +15,9 @@ function App() {
   return (
     <div>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Button onClick={() => setCount(count + 1)}>Click {count}</Button>
     </div>
   )
 }
 
-export default App
+export default App  
