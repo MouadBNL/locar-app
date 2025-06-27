eaab3badfbea376f2e5dacbcfcc76ee46fff35d3
MouadBNL <mouadbnl.2k01@gmail.com>
2025-06-27 13:11:28 +0100

    feat: building the api-client for hono

---
+++ packages/api-client/src/index.ts
@@ -0,0 +1,9 @@
+import { hc } from 'hono/client'
+import { AppType } from "@locar/api"
+
+
+const client = hc<AppType>("");
+export type Client = typeof client;
+
+export default (...args: Parameters<typeof hc>): Client =>
+    hc<AppType>(...args);
