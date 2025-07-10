0b9cde6a6e6a769f7f2f36f09fec642d5d4cd7b8
MouadBNL <mouadbnl.2k01@gmail.com>
2025-07-10 20:37:49 +0100

    chore: cleaning the old mess 2

--- packages/api-client/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { hc } from "hono/client";
-import { AppType } from "@locar/api/router";
-
-const client = hc<AppType>("");
-export type Client = typeof client;
-
-export default (...args: Parameters<typeof hc>): Client => hc<AppType>(...args);
