1bc933cbd258d56b6974231b2d1a1ab1001bc36e
MouadBNL <mouadbnl.2k01@gmail.com>
2025-07-11 15:57:51 +0100

    chore: minor changes

--- web/src/lib/http.ts
+++ web/src/lib/http.ts
@@ -1,25 +1,4 @@
-import api_client from "@locar/api-client";
-
-export const http = api_client("http://localhost:5173", {
-  headers() {
-    const headers: Record<string, string> = {
-      "Content-Type": "application/json",
-    };
-
-    return headers;
-  },
-  fetch: ((input, init) => {
-    return fetch(input, {
-      ...init,
-      credentials: "include", // Required for sending cookies cross-origin
-    }).then(async (res) => {
-      // console.log({ res });
-      if (res.status >= 400) {
-        const err = await res.json();
-        throw new Error(res.statusText, { cause: err });
-      }
-
-      return res;
-    });
-  }) satisfies typeof fetch,
-});
+import axios from "axios";
+export const http = axios.create({
+  baseURL: "http://localhost:8000/api/v1",
+})
