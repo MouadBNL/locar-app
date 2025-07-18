b689b7f1854f39ae7219f769a212b309f6be7875
MouadBNL <mouadbnl.2k01@gmail.com>
2025-07-18 14:56:44 +0100

    chore: added a footer to the app layout

--- web/src/components/layouts/app/app-layout.tsx
+++ web/src/components/layouts/app/app-layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppHeader } from "./app-header";
 import { AppSidebar } from "./_components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,6 +11,20 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
         <AppHeader />
 
         <main className="p-0 h-full">{children}</main>
+
+        <footer className="mt-8">
+          <div className="container mx-auto">
+            <Separator />
+            <div className="flex items-center justify-between text-muted-foreground py-4 text-sm ">
+              <p>© 2025 - All rights reserved</p>
+              <p>
+                <a href="#" className="hover:underline">
+                  Random Link
+                </a>
+              </p>
+            </div>
+          </div>
+        </footer>
       </SidebarInset>
     </SidebarProvider>
   );
