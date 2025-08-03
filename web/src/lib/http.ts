01b18d5b7aa2a364e3d1f9bbf0331a9ea0be7090
MouadBNL <mouadbnl.2k01@gmail.com>
2025-08-03 18:25:51 +0100

    chore: env & build

--- web/src/lib/http.ts
+++ web/src/lib/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 http.defaults.withCredentials = true;
